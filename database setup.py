e79fd7a615212a6c086f763ea02e835fb1804687
moaazEl <141212828+moaazEl@users.noreply.github.com>
2024-09-05 00:32:44 +1000

    Add files via upload

--- database setup.py
+++ database setup.py
@@ -8,24 +8,25 @@ def create_customers_table():
     cur = con.cursor()
 
     cur.execute("""
-        create table customers (
-        customer_id integer primary key autoincrement,
+        create table customers(
+        customer_id integer PRIMARY KEY autoincrement,
         address text not null,
         first text not null,
         surname text not null,
         phone text not null,
         dob text,
-        tran integer,
-        email text not null
+        FOREIGN KEY (tran) REFERENCES sales(tran),
+        email text not null,
+        password text not null
         );""")
     reader = csv.reader(open('cus.csv', 'r'),delimiter=',')
     next(reader)
     for row in reader:
         print(row)
-        to_db = [ row[1], row[2], row[3], row[7], row[4], row[5], row[6]]
-        cur.execute('''INSERT INTO customers (first, surname, dob, email, phone, address, tran)
-                       VALUES ( ?, ?, ?, ?, ?, ?, ?);''', to_db)
-       
+        to_db = [ row[1], row[2], row[3], row[7], row[4], row[5], row[6], row[7]]
+        cur.execute('''INSERT INTO customers (first, surname, dob, email, phone, address, tran, password)
+                       VALUES ( ?, ?, ?, ?, ?, ?, ?, ?);''', to_db)
+    cur.execute("UPDATE cusomers SET password = 'Password@45';") 
     con.commit()
     con.close()
     return 'cus.csv processed and data inserted into database.'
@@ -55,12 +56,14 @@ def create_purchase_table():
 
     cur.execute("""
         create table sales(
-        tran integer primary key autoincrement,
-        customer_id integer not null,
-        product_id integer not null,
-        date text not null,
-        fprice float not null,
-        trev float not null);""")
+            tran INTEGER PRIMARY KEY AUTOINCREMENT,
+            customer_id INTEGER NOT NULL,
+            product_id INTEGER NOT NULL,
+            date TEXT NOT NULL,
+            fprice FLOAT NOT NULL,
+            trev FLOAT NOT NULL,
+            FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+            FOREIGN KEY (product_id) REFERENCES products(product_id));""")
     con.commit()
     con.close()
 
